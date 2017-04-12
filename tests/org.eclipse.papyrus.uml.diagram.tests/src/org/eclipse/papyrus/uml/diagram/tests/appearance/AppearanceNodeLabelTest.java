85abbb0b3ea65a71ced03bcc6540160deb3432f2
Benoit maggi <benoit.maggi@cea.fr>
2017-04-12 11:22:17 +0200

    Extract SysML 1.1 from Papyrus

---
+++ tests/org.eclipse.papyrus.uml.diagram.tests/src/org/eclipse/papyrus/uml/diagram/tests/appearance/AppearanceNodeLabelTest.java
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * Copyright (c) 2013, 2015 CEA LIST, Christian W. Damus, and others.
+ *    
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   CEA LIST - Initial API and implementation
+ *   Christian W. Damus - bug 464647
+ *   
+ /*****************************************************************************/
+package org.eclipse.papyrus.uml.diagram.tests.appearance;
+
+/**
+ * Abstract test suite for the appearance tests of a node label edit-part.
+ */
+public abstract class AppearanceNodeLabelTest extends AbstractAppearanceNodeTest {
+	// No specific API or implementation
+}
